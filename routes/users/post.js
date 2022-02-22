a2015ecdd488d4a3adaa3cc7daa2efe0bf73c31d
shekhar8897 <shekharshashank97@gmail.com>
2022-02-22 17:08:09 -0500

    LOgin API

--- routes/users/post.js
+++ routes/users/post.js
@@ -29,6 +29,50 @@ const { UserRole } = require('../../constants/application');
 
 
 
+router.post("/login",[
+    check("username").notEmpty().withMessage("Username is required.").trim().isAlphanumeric().withMessage("Username should be alphanumeric."),
+    check("password").notEmpty().withMessage("Password is required.").trim().isLength({min:6}).withMessage("Password must be at least 6 character long"),
+    ],async (req,res)=>{
+    let responseCode,responseMessage,responseData;
+    try {
+        const errors=validationResult(req);
+        if(!errors.isEmpty()){
+            responseCode=HTTPStatusCode.BAD_REQUEST;
+            responseData=errors;
+            responseMessage=HTTPStatusCode.BAD_REQUEST;
+        }else{
+            let dbResponse=await DB_UTILS.dbUtils.findByUserName(req.body.username)
+            if(dbResponse){
+                const isPasswordMatched=await bcrypt.compare(req.body.password,dbResponse.password); // MATCH PASSWORD
+                if(isPasswordMatched){
+                    const token=await dbResponse.createToken();              // CREATE TOKEN
+                    dbResponse.token=token;                         // ASSIGNING JWT TOKEN
+                    responseCode=HTTPStatusCode.OK;
+                    responseMessage=HTTPStatusCode.OK;
+                    responseData=dbResponse
+                    
+                }else{
+                    responseCode=HTTPStatusCode.NOT_FOUND;
+                    responseMessage=HTTPStatusCode.NOT_FOUND;
+                    responseData="PASSWORD DOESN'T MATCH."
+                }
+            }else{
+                responseCode=HTTPStatusCode.NOT_FOUND;
+                responseMessage=HTTPStatusCode.NOT_FOUND;
+                responseData="USER NOT FOUND."
+            }
+        }
+    } catch (error) {
+        responseCode = HTTPStatusCode.INTERNAL_SERVER_ERROR
+        responseMessage = HTTPStatusCode.INTERNAL_SERVER_ERROR;
+        responseData = error.toString();
+    }
+    finally{
+        return res.status(responseCode ).send({message:responseMessage,data:responseData})
+    }
+    
+})
+
 
 router.post("/signup", [
     check("email").notEmpty().withMessage("Email is Required.").trim().isEmail().withMessage("Please enter a valid Email."),
